06eba3167651416a0a15c14cff7a53f135a8594f
gabrielbaez04 <gabrielbaez04@gmail.com>
2018-07-14 01:05:14 -0400

    Adding dashboard components.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import WorkoutList from '../src/components/WorkoutList';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<WorkoutList />, document.getElementById('root'));
 registerServiceWorker();
