0b0de4934e0a8223fe9b1e42d35d4f8ab9fdb15e
gabrielbaez04 <gabrielbaez04@gmail.com>
2018-09-14 23:24:26 -0400

    Added state and navigation for play and workout back buttons.

--- src/components/Workout.js
+++ src/components/Workout.js
@@ -4,37 +4,9 @@ import {initializeCube,nextExercise,prevExercise} from '../utils/helper';
 class Workout extends React.Component {
     state = {
         currentSide:0,
-        exercises:[
-            {
-                name: ' 1 - Biceps Curls With Dumbbell',
-                description: "Hold two barbells, the arms are streched, the hands are on your side, the palms face inwards. Bend the arms and bring the weight with a fast movement up. At the same time, rotate your arms by 90 degrees at the very beginning of the movement. At the highest point, rotate a little the weights further outwards. Without a pause, bring them down, slowly. Don't allow your body to swing during the exercise, all work is done by the biceps, which are the only mucles that should move (pay attention to the elbows).",
-                extra: 'Fluid movements with no pauses at the top or the bottom',
-                images: ['https://wger.de/media/exercise-images/81/Biceps-curl-2.png.200x200_q85.png',
-                                    'https://wger.de/media/exercise-images/129/Standing-biceps-curl-2.png']
-            },
-            {
-                name: '2 - Biceps Curls',
-                description: "Hold two barbells, the arms are streched, the hands are on your side, the palms face inwards. Bend the arms and bring the weight with a fast movement up. At the same time, rotate your arms by 90 degrees at the very beginning of the movement. At the highest point, rotate a little the weights further outwards. Without a pause, bring them down, slowly. Don't allow your body to swing during the exercise, all work is done by the biceps, which are the only mucles that should move (pay attention to the elbows).",
-                extra: 'Fluid movements with no pauses at the top or the bottom',
-                images: ['https://wger.de/media/exercise-images/81/Biceps-curl-2.png.200x200_q85.png',
-                                    'https://wger.de/media/exercise-images/129/Standing-biceps-curl-2.png']
-            },
-            {
-                name: '3 - Biceps Curls With Dumbbell',
-                description: "Hold two barbells, the arms are streched, the hands are on your side, the palms face inwards. Bend the arms and bring the weight with a fast movement up. At the same time, rotate your arms by 90 degrees at the very beginning of the movement. At the highest point, rotate a little the weights further outwards. Without a pause, bring them down, slowly. Don't allow your body to swing during the exercise, all work is done by the biceps, which are the only mucles that should move (pay attention to the elbows).",
-                extra: 'Fluid movements with no pauses at the top or the bottom',
-                images: ['https://wger.de/media/exercise-images/81/Biceps-curl-2.png.200x200_q85.png',
-                                    'https://wger.de/media/exercise-images/129/Standing-biceps-curl-2.png']
-            },
-            {
-                name: '4 - Dumbbell',
-                description: "Hold two barbells, the arms are streched, the hands are on your side, the palms face inwards. Bend the arms and bring the weight with a fast movement up. At the same time, rotate your arms by 90 degrees at the very beginning of the movement. At the highest point, rotate a little the weights further outwards. Without a pause, bring them down, slowly. Don't allow your body to swing during the exercise, all work is done by the biceps, which are the only mucles that should move (pay attention to the elbows).",
-                extra: 'Fluid movements with no pauses at the top or the bottom',
-                images: ['https://wger.de/media/exercise-images/81/Biceps-curl-2.png.200x200_q85.png',
-                                    'https://wger.de/media/exercise-images/129/Standing-biceps-curl-2.png']
-            }
-        ]
-    } 
+        exercises : this.props.exercises
+    }
+    
     componentDidMount(){
         initializeCube();
     }
@@ -47,6 +19,9 @@ class Workout extends React.Component {
         this.setState({currentSide: this.state.currentSide-1 < 0 ? 0 : this.state.currentSide-1},
             () => {prevExercise(this.state.currentSide)});
       
+    }
+    onBackClick = () =>{
+        this.props.handleBackClick();
     }
     render() {
         return (
@@ -71,16 +46,28 @@ class Workout extends React.Component {
                             })}
                         </div>
                     </div>
-                    <div className="ui clearing segment workoutButtons ">
+                    <div className="ui clearing segment workoutButtons">
+                    
                     {this.state.currentSide!=this.state.exercises.length-1 &&
                             <button className="ui right floated icon button compact blue big" onClick={this.handleStepForwardClick}>
                                 <i className="icon step forward customSmallButton"></i>
                             </button>
 
                     }
+                    {this.state.currentSide==this.state.exercises.length-1 &&
+                            <button className="ui right floated icon button compact blue big" onClick={this.onBackClick}>
+                                <i className="icon undo customSmallButton"></i>
+                            </button>
+
+                    }
                     {this.state.currentSide!=0 &&
                             <button className="ui compact icon button blue big" onClick={this.handleStepBackwardClick}>
                                 <i className="icon step backward customSmallButton"></i>  
+                            </button>  
+                    }
+                    {this.state.currentSide==0 &&
+                            <button className="ui compact icon button blue big" onClick={this.onBackClick}>
+                                <i className="icon undo customSmallButton"></i>  
                             </button>  
                     }
                     </div>
--- src/components/WorkoutList.js
+++ src/components/WorkoutList.js
@@ -1,47 +1,51 @@
 import React from 'react';
 import PreviewContainer from './PreviewContainer';
-
+import {workoutsData} from '../workoutsTestingData';
+import Workout from './Workout';
 
 class WorkoutList extends React.Component {
+    state = {
+        selectedWorkout: null,
+        workouts : workoutsData()       
+    }
+    handleWorkoutClick = (workout) =>{
+        console.log(workout.exercises)
+        this.setState({selectedWorkout: workout});
+    }
+    handleBackClick = () =>{
+        this.setState({selectedWorkout: null});
+    }
     render() {
         return (
             <div>
-                <h2 className="ui center aligned icon header">
-                <i className="circular hand rock icon"></i>
-                Workout List
-                </h2>
-                <div className='ui segment compact workoutsContainer'>
-                    <PreviewContainer
-                        name='Biceps'
-                        isWorkout="true"
-                        images={['https://wger.de/media/exercise-images/81/Biceps-curl-2.png.200x200_q85.png',
-                                'https://wger.de/media/exercise-images/129/Standing-biceps-curl-2.png',
-                                'https://wger.de/media/exercise-images/3/Standing-biceps-curl-1.png',
-                                'https://wger.de/media/exercise-images/3/Standing-biceps-curl-1.png']}
-                    />
-                    <PreviewContainer
-                        name='Triceps'
-                        isWorkout="true"
-                        images={['https://wger.de/media/exercise-images/81/Biceps-curl-2.png.200x200_q85.png',
-                                'https://wger.de/media/exercise-images/129/Standing-biceps-curl-2.png']}
-                    />
-                        <PreviewContainer
-                        name='Biceps'
-                        isWorkout="true"
-                    images={['https://wger.de/media/exercise-images/81/Biceps-curl-2.png.200x200_q85.png',
-                                'https://wger.de/media/exercise-images/3/Standing-biceps-curl-1.png',
-                                'https://wger.de/media/exercise-images/3/Standing-biceps-curl-1.png']}
-                    />
-                    <PreviewContainer
-                        name='Triceps'
-                        isWorkout="true"
-                        images={['https://wger.de/media/exercise-images/81/Biceps-curl-2.png.200x200_q85.png',
-                                'https://wger.de/media/exercise-images/129/Standing-biceps-curl-2.png']}
-                    />
-                </div>
-                <button className="ui button blue fluid addButton">
-                    <i className="plus alternate icon big link"></i>
-                </button>
+                {!this.state.selectedWorkout &&
+                    <div>
+                        <h2 className="ui center aligned icon header">
+                        <i className="circular hand rock icon"></i>
+                        Workout List
+                        </h2>
+                        <div className='ui segment compact workoutsContainer'>
+                            {this.state.workouts.map((workout,index)=>{
+                                return (<PreviewContainer
+                                    key = {workout.id}
+                                    name={workout.name}
+                                    isWorkout="true"
+                                    workoutData={workout}
+                                    handleWorkoutClick={this.handleWorkoutClick}
+                                />)
+                            })}
+                        </div>
+                        <button className="ui button blue fluid addButton">
+                            <i className="plus alternate icon big link"></i>
+                        </button>
+                    </div>
+                }
+                {this.state.selectedWorkout 
+                    && <Workout 
+                            exercises={this.state.selectedWorkout.exercises}
+                            handleBackClick={this.handleBackClick}
+                        />
+                }
             </div>
         );
     }
