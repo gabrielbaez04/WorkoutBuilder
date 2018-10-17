0112ef28139787b765596591328bce88211a3df1
gabrielbaez04 <gabrielbaez04@gmail.com>
2018-10-16 23:13:57 -0400

    Fix issue #58

--- client/core/AuthMenu.js
+++ client/core/AuthMenu.js
@@ -5,6 +5,7 @@ import {Link, withRouter} from 'react-router-dom'
 import auth from './../auth/auth-helper'
 import IconButton from '@material-ui/core/IconButton';
 import {withStyles} from '@material-ui/core/styles'
+import OutsideClickHandler from 'react-outside-click-handler';
 
 const styles = theme => ({});
   
@@ -18,31 +19,36 @@ class AuthMenu extends React.Component{
 
     update = (props) =>{
         auth.signout(() => this.props.history.push('/'));
-        this.props.updateMenu();
-        
+        this.props.updateMenu();    
     }
 
     render(){
         const css = require('./Menu.css');
         const { history, classes ,theme } = this.props;
         return(
-            <div className={this.state.class} id="myTopnav" style={{backgroundColor:theme.palette.primary.main, }}>
-                <Link to={{pathname: "/user/" + this.props.authenticated.user._id, updateMenu: this.props.updateMenu}}
-                    style={this.props.style(history, "/user/" + this.props.authenticated.user._id)} 
-                    className="active"
-                    onClick={this.handleMenuClick}> My Profile </Link>
+            <OutsideClickHandler
+                onOutsideClick={() => {
+                    this.setState({ class:'topnav'});
+                }}
+              >
+                <div className={this.state.class} id="myTopnav" style={{backgroundColor:theme.palette.primary.main, }}>
+                    <Link to={{pathname: "/user/" + this.props.authenticated.user._id, updateMenu: this.props.updateMenu}}
+                        style={this.props.style(history, "/user/" + this.props.authenticated.user._id)} 
+                        className="active"
+                        onClick={this.handleMenuClick}> My Profile </Link>
 
-                <Link to="/routine" style={this.props.style(history, "/routine")}
-                    onClick={this.handleMenuClick}> Routines </Link>
+                    <Link to="/routine" style={this.props.style(history, "/routine")}
+                        onClick={this.handleMenuClick}> Routines </Link>
 
-                <a onClick={this.update}> Sign out </a>
-                
-                <a  className="icon" onClick={this.handleMenuClick}>
-                    {this.state.class == 'topnav' 
-                    ? <i className="fa fa-bars"></i>
-                    : <i className="fa fa-times"></i>}
-                </a>
-            </div>
+                    <a onClick={this.update}> Sign out </a>
+                    
+                    <a  className="icon" onClick={this.handleMenuClick}>
+                        {this.state.class == 'topnav' 
+                        ? <i className="fa fa-bars"></i>
+                        : <i className="fa fa-times"></i>}
+                    </a>
+                </div>
+            </OutsideClickHandler>
       )
     }
 
