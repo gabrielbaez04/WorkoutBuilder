2141c678f13b5553ee51fe06b7b5a67d055a841c
gabrielbaez04 <gabrielbaez04@gmail.com>
2019-03-05 17:00:29 -0400

    adding testing

--- __tests__/components/Signup.test.js
+++ __tests__/components/Signup.test.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { Signup } from '../../client/user/Signup';
+import "isomorphic-fetch"
 
 let wrapper, updateMenu;
 
-beforeEach(()=>{
+beforeEach(() => {
     updateMenu = jest.fn();
-    wrapper = shallow(<Signup classes={{}} location={{updateMenu}}/>);
+    wrapper = shallow(<Signup classes={{}} location={{ updateMenu }} />);
 });
 
-test('should render Signup correctly',()=>{
+test('should render Signup correctly', () => {
     expect(wrapper).toMatchSnapshot();
+});
+
+test('should set name on name change', () => {
+    const value = 'Gabriel';
+    wrapper.find('#name').prop('onChange')({ target: { value } });
+    expect(wrapper.state('name')).toBe(value);
+});
+
+test('should set email on email change', () => {
+    const value = 'Gabrielbaez04@gmail.com';
+    wrapper.find('#email').prop('onChange')({ target: { value } });
+    expect(wrapper.state('email')).toBe(value);
+});
+
+test('should set password on password change', () => {
+    const value = 'gabriel123';
+    wrapper.find('#password').prop('onChange')({ target: { value } });
+    expect(wrapper.state('password')).toBe(value);
 });
--- client/user/Signup.js
+++ client/user/Signup.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
@@ -7,14 +7,14 @@ import TextField from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography'
 import Icon from '@material-ui/core/Icon'
 import PropTypes from 'prop-types'
-import {withStyles} from '@material-ui/core/styles'
-import {create} from './api-user.js'
+import { withStyles } from '@material-ui/core/styles'
+import { create } from './api-user.js'
 import Dialog from '@material-ui/core/Dialog'
 import DialogActions from '@material-ui/core/DialogActions'
 import DialogContent from '@material-ui/core/DialogContent'
 import DialogContentText from '@material-ui/core/DialogContentText'
 import DialogTitle from '@material-ui/core/DialogTitle'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const styles = theme => ({
   card: {
@@ -22,8 +22,8 @@ const styles = theme => ({
     margin: 'auto',
     textAlign: 'center',
     paddingTop: 5,
-    paddingLeft:5,
-    paddingRight:5,
+    paddingLeft: 5,
+    paddingRight: 5,
     marginTop: '24px'
   },
   [theme.breakpoints.up('sm')]: {
@@ -51,15 +51,15 @@ const styles = theme => ({
 
 export class Signup extends Component {
   state = {
-      name: '',
-      password: '',
-      email: '',
-      open: false,
-      error: ''
+    name: '',
+    password: '',
+    email: '',
+    open: false,
+    error: ''
   }
 
   handleChange = name => event => {
-    this.setState({[name]: event.target.value})
+    this.setState({ [name]: event.target.value })
   }
 
   clickSubmit = (e) => {
@@ -71,15 +71,15 @@ export class Signup extends Component {
     }
     create(user).then((data) => {
       if (data.error) {
-        this.setState({error: data.error})
+        this.setState({ error: data.error })
       } else {
-        this.setState({error: '', open: true})
+        this.setState({ error: '', open: true })
       }
     })
   }
 
   render() {
-    const {classes} = this.props
+    const { classes } = this.props
     return (
       <div>
         <form onSubmit={this.clickSubmit} className="form">
@@ -88,10 +88,10 @@ export class Signup extends Component {
               <Typography type="headline" component="h2" className={classes.title}>
                 Sign Up
               </Typography>
-              <TextField id="name" label="Name" className={classes.textField} value={this.state.name} onChange={this.handleChange('name')} margin="normal"/><br/>
-              <TextField id="email" type="email" label="Email" className={classes.textField} value={this.state.email} onChange={this.handleChange('email')} margin="normal"/><br/>
-              <TextField id="password" type="password" label="Password" className={classes.textField} value={this.state.password} onChange={this.handleChange('password')} margin="normal"/>
-              <br/> {
+              <TextField id="name" label="Name" className={classes.textField} value={this.state.name} onChange={this.handleChange('name')} margin="normal" /><br />
+              <TextField id="email" type="email" label="Email" className={classes.textField} value={this.state.email} onChange={this.handleChange('email')} margin="normal" /><br />
+              <TextField id="password" type="password" label="Password" className={classes.textField} value={this.state.password} onChange={this.handleChange('password')} margin="normal" />
+              <br /> {
                 this.state.error && (<Typography component="p" color="error">
                   <Icon color="error" className={classes.error}>error</Icon>
                   <span className="spanError">{this.state.error}</span></Typography>)
@@ -110,14 +110,14 @@ export class Signup extends Component {
             </DialogContentText>
           </DialogContent>
           <DialogActions>
-            <Link to={{pathname: "/signin", updateMenu: this.props.location.updateMenu}}>
+            <Link to={{ pathname: "/signin", updateMenu: this.props.location.updateMenu }}>
               <Button color="primary" autoFocus="autoFocus" variant="raised">
                 Sign In
               </Button>
             </Link>
           </DialogActions>
         </Dialog>
-    </div>)
+      </div>)
   }
 }
 
